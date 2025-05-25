7b99c439f4352c19d70f43907ea81d95b792a546
Himanshu Kumar <himanshusinha383@gmail.com>
2025-05-25 14:00:03 +0530

    add array question solution

---
+++ DSA/array/5_189_rotate_array.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>    
+using namespace std;
+
+
+
+class Solution {
+public:
+    void reverse(vector<int>& nums, int start , int end){
+        while(start<=end){
+            int temp = nums[start];
+            nums[start ]= nums[end];
+            nums[end]= temp;
+            start++;
+            end--;
+        }
+    }
+
+    void rotate(vector<int>& nums, int k) {
+        int array_size = nums.size();
+                k =k%array_size;
+        if(k>array_size){
+            return;
+        }
+        //reverse last k element
+        reverse(nums,array_size -k,array_size -1);
+        //reverse start to last k element
+        reverse(nums,0,array_size -k-1);
+        //reverse the whole array
+        reverse(nums,0,array_size -1);
+    }
+};
